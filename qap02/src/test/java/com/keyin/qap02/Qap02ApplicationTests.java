e60ba0e2b062e584d15dce799786023e3fb63a43
Matt Murphy <45915909+Mattmurphy97@users.noreply.github.com>
2022-12-05 21:03:25 -0330

    Add files via upload

---
+++ qap02/src/test/java/com/keyin/qap02/Qap02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.qap02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qap02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
